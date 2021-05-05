a093c695db349240725556e9cce95567de0f0488
Shamsiddin <shamsiddin2000.09@gmail.com>
2021-05-05 11:31:00 +0200

    separated managers in file structure

---
+++ accounts/managers.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+# Create your model managers here
+
+
+class CustomUserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def _create_user(self, phone, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not phone:
+            raise ValueError('The given phone must be set')
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, phone, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(phone, password, **extra_fields)
+
+    def create_superuser(self, phone, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(phone, password, **extra_fields)
--- accounts/models.py
+++ accounts/models.py
@@ -1,56 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import ugettext_lazy as _
-
-ROLE_CHOICES = (
-    (1, "Admin"),
-    (2, "Teacher"),
-    (3, "Student")
-)
-
-# Create your model managers here
-
-
-class CustomUserManager(BaseUserManager):
-
-    use_in_migrations = True
-
-    def _create_user(self, phone, password, **extra_fields):
-        """Create and save a User with the given email and password."""
-        if not phone:
-            raise ValueError('The given phone must be set')
-        user = self.model(phone=phone, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, phone, password=None, **extra_fields):
-        """Create and save a regular User with the given email and password."""
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(phone, password, **extra_fields)
-
-    def create_superuser(self, phone, password, **extra_fields):
-        """Create and save a SuperUser with the given email and password."""
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(phone, password, **extra_fields)
-
+from .managers import CustomUserManager
 
 # Create your models here.
 
+
 class Account(AbstractUser):
+    ROLE_CHOICES = (
+        (1, "Admin"),
+        (2, "Teacher"),
+        (3, "Student")
+    )
     username = None
     passport_number = models.CharField(max_length=10, unique=True, null=True)
     phone = models.CharField(max_length=30, unique=True)
-    role = models.PositiveSmallIntegerField(
-        choices=ROLE_CHOICES, blank=True, null=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True)
 
     USERNAME_FIELD = 'phone'
     REQUIRED_FIELDS = []
