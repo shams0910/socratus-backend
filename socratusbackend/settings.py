56f6f9d3bbf132e6f9c4b76fb92d673d2bc38fc1
Shamsiddin <shamsiddin2000.09@gmail.com>
2021-05-05 11:49:53 +0200

    deleted unuseful apps in project

--- socratusbackend/settings.py
+++ socratusbackend/settings.py
@@ -38,11 +38,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
@@ -50,13 +47,13 @@ INSTALLED_APPS = [
     'tenants'
 ]
 
+AUTH_USER_MODEL = 'accounts.Account'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'tenants.middlewares.TenantMiddleware'
 ]
@@ -73,7 +70,6 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
